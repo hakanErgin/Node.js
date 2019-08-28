aa024be5f86af69cfd1f0a6d52ff079d83221802
hakanErgin <hakan.ergin@gmail.com>
2019-08-28 19:26:08 +0200

    homework

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,8 +10,77 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
-  });
+    const url = request.url;
 
+    if (url === "/state") {
+      response.writeHead(
+        200,
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      const stateResponse = {
+        state
+      }
+      response.write(JSON.stringify(stateResponse));
+      response.end()
+    }
+    else if (url === "/add") {
+      response.writeHead(
+        200,
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      state++;
+      const stateResponse = {
+        "state": state
+      }
+      response.write(JSON.stringify(stateResponse));
+      response.end()
+    }
+    else if (url === "/subtract") {
+      response.writeHead(
+        200,
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      state--;
+      const stateResponse = {
+        "state": state
+      }
+      response.write(JSON.stringify(stateResponse));
+      response.end()
+    }
+    else if (url === "/reset") {
+      response.writeHead(
+        200,
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      state = 10;
+      const stateResponse = {
+        state
+      }
+      response.write(JSON.stringify(stateResponse));
+      response.end()
+    }
+    else {
+      response.writeHead(
+        404,
+        {
+          'Content-Type': 'application/json'
+        }
+      );
+      const stateResponse = {
+        "error": "Not found"
+      }
+      response.write(JSON.stringify(stateResponse));
+      response.end()
+    }
+  });
   return server;
 }
 
