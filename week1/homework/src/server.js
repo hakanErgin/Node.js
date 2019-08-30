a24d4ffe5e145bed428e73d5ebc97316fe73946b
hakanErgin <hakan.ergin@gmail.com>
2019-08-30 10:22:01 +0200

    updated a little more. didn't refactor using switch, because then it will be identical with other homework here :) I get the point tho

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -7,57 +7,34 @@ const http = require('http');
  */
 function createServer(port) {
   let state = 10;
-
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
     const url = request.url;
-
+    response.writeHead(200, { 'Content-Type': 'application/json' });
     if (url === "/state") {
-      response.writeHead(
-        200,
-        { 'Content-Type': 'application/json' }
-      );
       response.write(JSON.stringify({ state }));
     }
     else if (url === "/add") {
-      response.writeHead(
-        200,
-        { 'Content-Type': 'application/json' }
-      );
       state++;
       response.write(JSON.stringify({ state }));
     }
     else if (url === "/subtract") {
-      response.writeHead(
-        200,
-        { 'Content-Type': 'application/json' }
-      );
       state--;
       response.write(JSON.stringify({ state }));
     }
     else if (url === "/reset") {
-      response.writeHead(
-        200,
-        { 'Content-Type': 'application/json' }
-      );
       state = 10;
       response.write(JSON.stringify({ state }));
     }
     else {
-      response.writeHead(
-        404,
-        { 'Content-Type': 'application/json' }
-      );
-      const stateResponse = {
-        "error": "Not found"
-      }
+      response.writeHead(404, { 'Content-Type': 'application/json' });
+      const stateResponse = { "error": "Not found" }
       response.write(JSON.stringify(stateResponse));
     }
     response.end()
   });
   return server;
 }
-
 module.exports = {
   createServer
 };
