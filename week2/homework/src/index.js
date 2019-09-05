8ef522fbb152bf9d0e12a093181eefd9568da814
hakanErgin <hakan.ergin@gmail.com>
2019-09-05 12:11:54 +0200

    working on week 2 HW

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,34 @@
 'use strict';
 
+const funcs = require("./funcs");
+
 // TODO: Write the homework code in this file
+const usableArgs = process.argv.slice(2)
+const command = usableArgs[0];
+const firstParam = usableArgs[1]
+const secondParam = usableArgs[2]
+
+
+switch (command) {
+    case 'help':
+        funcs.showHelp();
+        break;
+    case 'list':
+        funcs.showList();
+        break;
+    case 'add':
+        funcs.add(firstParam);
+        break;
+    case 'remove':
+        funcs.remove(firstParam);
+        break;
+    case 'update':
+        funcs.update(firstParam, secondParam);
+        break;
+    case 'reset':
+        funcs.resetList();
+        break;
+    default:
+        funcs.showHelp();
+    // throw new Error('Command not supported');
+}
---
+++ week2/homework/src/todolist.js
@@ -0,0 +1,7 @@
+'use strict';
+
+let todoList = ["x", "y", "z"];
+
+module.exports = {
+    todoList,
+}
